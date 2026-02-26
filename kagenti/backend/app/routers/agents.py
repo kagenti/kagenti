1cbed4e7fdd34a7f6a4fce303d27d7d2e549a904
usize <mofoster@redhat.com>
2026-02-25 16:18:01 -0800

    Add support for multiple protocols via the protocol.kagenti.io/<proto> label
    Signed-off-by: usize <mofoster@redhat.com>

--- kagenti/backend/app/routers/agents.py
+++ kagenti/backend/app/routers/agents.py
@@ -1895,6 +1895,8 @@ def _build_common_labels(
     Returns:
         Dictionary of labels.
     """
+    from app.core.constants import PROTOCOL_LABEL_PREFIX
+
     labels = {
         # Required labels
         KAGENTI_TYPE_LABEL: RESOURCE_TYPE_AGENT,
